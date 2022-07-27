a99494ec403e035fec4ccbe6eb495932997f7e6a
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-07-27 14:09:48 +0530

    Added: Pring message using one way data binding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-      <h1> Hello from Bridgelabz</h1>
-      </div>
-      );
+class App extends React.Component { 
+  constructor(){
+    super()
+    this.state= {
+      title: "Hello from Bridgelabz"
     }
   }
+  render(){
+  return (
+    <div className="App">
+      <h1>{this.state.title}</h1>
+    </div>
+  );
+  }
+}
 
 export default App;
