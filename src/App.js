8f103a7c29f8e2469ac4532f2c35f69b9b42faf8
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-07-27 16:33:29 +0530

    Added: Adding the User Input name.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(){
     super()
     this.state= {
-      title: "Hello from Bridgelabz"
+      userName: ''
     }
   }
 
@@ -16,13 +16,23 @@ class App extends React.Component {
     console.log("save button is clicked", $event)
     window.open(this.url, "_blank");
   }
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    //set the title using setState method
+    this.setState({userName: event.target.value})
+  }
   render(){
   return (
-    <div className="App">
-      <h1>{this.state.title}</h1>
+    <>
+    <div>
+      <h1>Hello {this.state.userName} from Bridgelabz</h1>
       <img src={logo} onClick={this.onClick}
       alt ="The Bridgelabz logo: a Bridge to Employment through lab works" />
     </div>
+    <div>
+      <input onChange={this.onNameChange} />
+    </div>
+    </>
   );
   }
 }
